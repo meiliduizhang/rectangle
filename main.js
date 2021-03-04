f1dd5a23735c661019be9fc5c6f3a8ceccd32bb1
Charismatic Captain <chenkaihua2020@163.com>
2021-03-04 12:23:06 +0800

    矩形计算器v.0.1

--- main.js
+++ main.js
@@ -5,16 +5,48 @@ $(()=>{
       $btnCal = $('#calc'),
       $perimeter = $('#perimeter'),
       $area = $('#area');
+  //判断是是否是浮点数，如果是，求出是几位
+  function roundFractional(x, n) {
+    return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+  }
+  function ifFloat(num){
+    let flag = 0;
+    let y = String(num).indexOf('.')+1;
+    var count = String(num).length - y;
+    if(y > 0 && count > 0){
+      return count;//返回几位小数
+    }else{
+      return flag;
+    }
+  }
   //calc button click
   $btnCal.click(()=>{
    //get value
+   let num;
+   let Num;
    let w = Number($width.val()),
        h = Number($height.val());
+   let num1 = ifFloat(w);
+   let num2 = ifFloat(h);
+   if(num1 == num2) {
+    num = num1;
+    Num=num1+num2;
+   }
+   else if(num1 > num2){
+     num = num1;
+     Num=num1+num2;
+   }
+   else if(num1 < num2){
+     num = num2;
+     Num=num1+num2;
+   }
    //calc
    let p = (w+h)*2,
        a = w * h;
    //output
-   $perimeter.val(p);
-   $area.val(a);
+   let answer1 = roundFractional(p,num);
+   let answer2 = roundFractional(a,Num);
+    $perimeter.val(answer1);
+    $area.val(answer2);
   });
 });
